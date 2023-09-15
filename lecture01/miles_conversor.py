394bcce6e5ebc4800a2c4ddc7ed55d99a6599c60
Pablo Vitor <thevitor00@gmail.com>
2023-09-14 23:26:42 -0300

    Changes through all the base course due to teaching methods

---
+++ lecture01/miles_conversor.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as ttk
+
+def convert():
+    mile_imput = entry_int.get()
+    km_output = mile_imput * 1.61
+    output_string.set(km_output)
+
+#window
+window = ttk.Window(themename = 'journal') #journal/ darkly
+window.title("Demo")
+window.geometry("300x150")
+
+#title
+title_label = ttk.Label(master = window, text = "Miles to Kilometers", font= "Calibri 24 bold")
+title_label.pack()
+
+#input field
+input_frame = ttk.Frame(master = window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master = input_frame, textvariable = entry_int)
+button = ttk.Button(master = input_frame, text = "Convert", command = convert)
+entry.pack(side = "left", padx = 10)
+button.pack(side = "left")
+input_frame.pack(pady = 10)
+
+#output
+output_string = tk.StringVar()
+output_label = ttk.Label(master = window, text = "Output", font = "Calibri 24", textvariable = output_string)
+output_label.pack(pady = 5)
+
+#run
+window.mainloop()
